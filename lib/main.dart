738118e326cb76a29e33bea532384865ecd0ec2d
Thallys Braz <thallysbraz3@gmail.com>
2024-06-03 20:24:28 -0300

    feat: fifinalizando implementação do primeiro aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,70 +11,113 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key ? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
 
   void decrement() {
-    print("Decrement");
+    setState(() {
+      count--;
+    });
+    print(count);
   }
 
   void increment(){
-    print('Incrementou');
+    setState(() {
+      count++;
+    });
+    print(count);
   }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 10;
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: Colors.red,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Pode Entrar!',
-              style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-            fontWeight: FontWeight.w700,
-            )
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/imageFood.jpg'),
+            fit: BoxFit.cover,
           ),
-          const Text(
-            '0',
-            style: TextStyle(
-            fontSize: 100,
-            color: Colors.white,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isFull ? 'Lotado!' : 'Pode Entrar!',
+              style: const TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              )
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                  onPressed: decrement,
-                  child: Text(
-                    'Saiu',
-                    style:
-                      TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
+            Padding(
+              padding: EdgeInsets.all(40),
+              child: Text(
+                count.toString(),
+                style: const TextStyle(
+                  fontSize: 100,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: isEmpty ? null : decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
                     ),
                   ),
-              ),
-              TextButton(
-                onPressed: increment,
-                child: Text(
-                  'Entrou',
-                  style: TextStyle(
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(
                       color: Colors.black,
-                    fontSize: 16,
+                      fontSize: 16,
                     ),
+                  ),
                 ),
-              )
-            ],
-          )
-        ],
+                const SizedBox(width: 32),
+                TextButton(
+                  onPressed: isFull ? null : increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
